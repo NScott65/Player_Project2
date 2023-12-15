c8087f2f21fb503415664376dfe26d58fd079b58
Noah <scott.613865@swl.k12.oh.us>
2023-12-15 09:46:33 -0500

    h

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,134 @@
+
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        Player player1 = new Player();
+
+
+
+        String name = "";
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        String name2 = "";
+        int x2 = 0;
+        int y2 = 0;
+        int z2 = 0;
+        String name3 = "";
+        int x3 = 0;
+        int y3 = 0;
+        int z3 = 0;
+        int hp3 = 0;
+        int d3 = 0;
+        boolean done = false;
+        String choice = "";
+
+        System.out.println("Welcome to The Game!!\nHere is your first Player:\n");
+        System.out.println(player1 + "\n");
+        System.out.println("Now enter the Name and Coordinates of the second player: ");
+        System.out.println("Name: ");
+        if(scan.hasNextLine()){
+            name2 = scan.nextLine();
+        }
+        System.out.println("X: ");
+        if(scan.hasNextInt()){
+            x2 = scan.nextInt();
+        }
+        System.out.println("Y: ");
+        if(scan.hasNextInt()){
+            y2 = scan.nextInt();
+        }
+        System.out.println("Z: ");
+        if(scan.hasNextInt()){
+            z2 = scan.nextInt();
+        }
+        Player player2 = new Player(name2, x2, y2, z2);
+        System.out.println("\n" + player2);
+
+        scan.nextLine();
+        System.out.println("\nNow enter the Name, Coordinates, HP, and Direction the third player is facing: ");
+        System.out.println("Name: ");
+        if(scan.hasNextLine()){
+            name3 = scan.nextLine();
+        }
+        System.out.println("X: ");
+        if(scan.hasNextInt()){
+            x3 = scan.nextInt();
+        }
+        System.out.println("Y: ");
+        if(scan.hasNextInt()){
+            y3 = scan.nextInt();
+        }
+        System.out.println("Z: ");
+        if(scan.hasNextInt()){
+            z3 = scan.nextInt();
+        }
+        System.out.println("HP: ");
+        if(scan.hasNextInt()){
+            hp3 = scan.nextInt();
+        }
+        System.out.println("Direction: ");
+        if(scan.hasNextInt()){
+            d3 = scan.nextInt();
+        }
+        Player player3 = new Player(name3, x3, y3, z3, hp3, d3);
+        System.out.println("\n" + player3);
+        scan.nextLine();
+
+        do{
+            String yn = getYNConfirm(scan, "Has anything happened in the game?[Y/N]");
+            System.out.println(yn);
+            if(yn.equalsIgnoreCase("true")){
+                System.out.println("Has anyone been attacked or been moved?(please enter the words exactly as previewed)");
+                if(scan.hasNextLine()){
+                    choice = scan.nextLine();
+                }else{
+                    System.out.println("your input was invalid.");
+                }
+                if(choice.equalsIgnoreCase("attacked")){
+
+                }else if(choice.equalsIgnoreCase("moved")){
+
+                }
+            }
+
+        }while(!done);
+
+
+    }
+    public static String getYNConfirm(Scanner in, String prompt){
+        String input = " ";
+        boolean yn = false;
+        boolean valid = false;
+        String end = "";
+
+        System.out.println(prompt);
+
+        do{
+            if(in.hasNextLine()){
+                input = in.nextLine();
+                if(input.equalsIgnoreCase("Y")){
+                    yn = true;
+                    valid = true;
+                }else if(input.equalsIgnoreCase("N")){
+                    yn = false;
+                    valid = true;
+                }else{
+                    System.out.println("Your input is invalid.");
+                }
+            }else{
+                System.out.println("Your input is invalid.");
+            }
+        }while(!valid);
+
+        if(yn == true){
+            input = "true";
+        }else if(yn == false){
+            input = "false";
+        }
+
+        return input;
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,135 @@
-package PACKAGE_NAME;public class Player {
+import java.util.Scanner;
+public class Player {
+    Scanner scan = new Scanner(System.in);
+
+    private int x;
+    private int y;
+    private int z;
+    private int direction;
+    private int hp;
+    private String name;
+    private int pCount = 1;
+
+
+    public Player(){
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.hp = 20;
+        this.name = ("P" + pCount);
+    }
+
+    public Player(String name, int x, int y, int z ){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.hp = 20;
+        this.direction = 1;
+    }
+
+    public Player(String name, int x, int y, int z, int hp, int direction){
+        String d = "";
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.hp = hp;
+        this.direction = direction;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+    public int getZ(){
+        return z;
+    }
+
+    public int getHp(){
+        return hp;
+    }
+
+    public int getDirection(){
+        return direction;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getpCount(){
+        return pCount;
+    }
+
+
+    public String toString(){
+        String out = "";
+        if(direction >= 1 && direction <= 6){
+            out = ("Name: " + name + "\nHP: " + hp + "\nX: " + x + " Y: " + y + " Z: " + z + "\nDirection: " + direction);
+        }else{
+            out = ("Name: " + name + "\nHP: " + hp + "\nX: " + x + " Y: " + y + " Z: " + z);
+        }
+        return out;
+    }
+
+    //void methods
+    public void setHp(int hp){
+        if(hp >= 0 && hp <= 100){
+            this.hp = hp;
+        }
+        else{
+            hp = 20;
+        }
+    }
+    public void setDirection(int direction){
+        if(direction >= 1 && direction <= 6){
+            this.direction = direction;
+        }
+        else{
+            direction = 1;
+        }
+    }
+    public void move(int direction, int unit){
+        if(direction >= 1 && direction <= 6){
+            this.direction = direction;
+        }
+        else{
+            direction = 1;
+        }
+        if(direction == 1){
+            this.x += unit;
+        }
+        else if(direction == 2){
+            this.x -= unit;
+        }
+        else if(direction == 3){
+            this.y += unit;
+        }
+        else if(direction == 4){
+            this.y -= unit;
+        }
+        else if(direction == 5){
+            this.z += unit;
+        }
+        else if(direction == 6){
+            this.z -= unit;
+        }
+    }
+    public void Tp(int x,int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public void TpPlayer(Player player){
+        this.x = player.x;
+        this.y = player.y;
+        this.z = player.z;
+    }
+    public void attack(Player player, int damage){
+        player.hp -= damage;
+    }
 }
