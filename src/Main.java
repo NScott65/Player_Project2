2f7ed92c0ef5b3575f96dbccfcd8227dd27b6018
NScott65 <144457750+NScott65@users.noreply.github.com>
2023-12-18 19:00:52 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,8 @@ public class Main {
         boolean done = false;
         String choice = "";
 
-        System.out.println("Welcome to The Game!!\nHere is your first Player:\n");
+        System.out.println("Welcome to The Game!!\nHere is the list for directions to # (North = 1, South = 2, Up = 3, Down = 4, East = 5, West = 6)\nHere is your first Player:\n");
+        System.out.println("\nX is (+)North and (-)South, Y is (+)Up and (-)Down, Z is (+)East and (-)West.");
         System.out.println(player1 + "\n");
         System.out.println("Now enter the Name and Coordinates of the second player: ");
         System.out.println("Name: ");
@@ -93,21 +94,141 @@ public class Main {
                     int finalAttack = 0;
                     if(attackedPlayer.equalsIgnoreCase("P1")){
                         Player.attack(player1, attackAmount);
+                        System.out.println("Here is an updated profile.\n");
                         System.out.println(player1);
                     }else if(attackedPlayer.equalsIgnoreCase("P2")){
                         Player.attack(player2, attackAmount);
+                        System.out.println("Here is an updated profile.\n");
                         System.out.println(player2);
                     }else if(attackedPlayer.equalsIgnoreCase("P3")){
                         Player.attack(player3, attackAmount);
+                        System.out.println("Here is an updated profile.\n");
                         System.out.println(player3);
                     }
 
-                }else if(choice.equalsIgnoreCase("moved")){
+                }else if(choice.equalsIgnoreCase("move")){
+                    String movedPlayer = getNonZeroLengString(scan, "Which player was attacked?(P1, P2, P3)");
+                    int direction = getInt(scan, "Which direction is the player moving?(1-6)");
+                    int distance = getInt(scan, "How far is the Player moving?");
+                    if(movedPlayer.equalsIgnoreCase("P1")){
+                      Player.move(player1,direction, distance);
+                        System.out.println("Here is an updated profile.\n");
+                        System.out.println(player1);
+                    }else if(movedPlayer.equalsIgnoreCase("P2")){
+                        Player.move(player2,direction, distance);
+                        System.out.println("Here is an updated profile.\n");
+                        System.out.println(player2);
+                    }else if(movedPlayer.equalsIgnoreCase("P3")){
+                        Player.move(player3,direction, distance);
+                        System.out.println("Here is an updated profile.\n");
+                        System.out.println(player3);
+                    }
+
 
                 }else if(choice.equalsIgnoreCase("teleport")){
+                    String type = getNonZeroLengString(scan, "If a player is teleporting to a certain coordinate enter (C) if they are going to another player enter (P).");
+                    if(type.equalsIgnoreCase("c")){
+                        String tPlayer = getNonZeroLengString(scan,"Which player is being teleported?(P1, P2, P3)");
+                        System.out.println("Enter the X, Y, and Z coordinates the player is being teleported to.");
+                        int tX = getInt(scan, "X: ");
+                        int tY = getInt(scan, "Y: ");
+                        int tZ = getInt(scan, "Z: ");
+                        if(tPlayer.equalsIgnoreCase("p1")) {
+                            Player.Tp(player1, tX, tY, tZ);
+                            System.out.println("Here is an updated profile.\n");
+                            System.out.println(player1);
+                        }else if(tPlayer.equalsIgnoreCase("p2")) {
+                            Player.Tp(player2, tX, tY, tZ);
+                            System.out.println("Here is an updated profile.\n");
+                            System.out.println(player2);
+                        }else if(tPlayer.equalsIgnoreCase("p3")) {
+                            Player.Tp(player3, tX, tY, tZ);
+                            System.out.println("Here is an updated profile.\n");
+                            System.out.println(player3);
+                        }
+                    }else if(type.equalsIgnoreCase("p")){
+                        String playerA = getNonZeroLengString(scan,"Which player is teleporting?(P1, P2, P3)");
+                        String playerB = getNonZeroLengString(scan, "Which player is being teleported to(P1, P2, P3)?");
+                        if(playerA.equalsIgnoreCase("p1") && playerB.equalsIgnoreCase("p2")){
+                            Player.TpPlayer(player1, player2);
+                            System.out.println("Here is an updated profile for both players.\n");
+                            System.out.println(player1 + "\n");
+                            System.out.println(player2);
+                        }else if(playerA.equalsIgnoreCase("p1") && playerB.equalsIgnoreCase("p3")){
+                            Player.TpPlayer(player1, player3);
+                            System.out.println("Here is an updated profile for both players.\n");
+                            System.out.println(player1 + "\n");
+                            System.out.println(player3);
+                        }else if(playerA.equalsIgnoreCase("p2") && playerB.equalsIgnoreCase("p1")){
+                            Player.TpPlayer(player2, player1);
+                            System.out.println("Here is an updated profile for both players.\n");
+                            System.out.println(player2 + "\n");
+                            System.out.println(player1);
+                        }else if(playerA.equalsIgnoreCase("p2") && playerB.equalsIgnoreCase("p3")){
+                            Player.TpPlayer(player2, player3);
+                            System.out.println("Here is an updated profile for both players.\n");
+                            System.out.println(player2 + "\n");
+                            System.out.println(player3);
+                        }else if(playerA.equalsIgnoreCase("p3") && playerB.equalsIgnoreCase("p1")){
+                            Player.TpPlayer(player3, player1);
+                            System.out.println("Here is an updated profile for both players.\n");
+                            System.out.println(player3 + "\n");
+                            System.out.println(player1);
+                        }else if(playerA.equalsIgnoreCase("p3") && playerB.equalsIgnoreCase("p2")){
+                            Player.TpPlayer(player3, player2);
+                            System.out.println("Here is an updated profile for both players.\n");
+                            System.out.println(player3 + "\n");
+                            System.out.println(player2);
+                        }else if(playerA.equalsIgnoreCase(playerB)){
+                            System.out.println("Please choose to different players.");
+                        }
+                    }
 
                 }else if(choice.equalsIgnoreCase("get a distance") || choice.equalsIgnoreCase("get distance") || choice.equalsIgnoreCase("distance")){
-
+                    String type = getNonZeroLengString(scan, "If a distance for a certain Player enter (C) if you want the distance from one player to another enter (P).");
+                    if(type.equalsIgnoreCase("c")){
+                        String tPlayer = getNonZeroLengString(scan,"Which player are you getting the distance of?(P1, P2, P3)");
+                        System.out.println("Enter the X, Y, and Z coordinates you are trying to get the distance from.");
+                        int tX = getInt(scan, "X: ");
+                        int tY = getInt(scan, "Y: ");
+                        int tZ = getInt(scan, "Z: ");
+                        double distance = 0;
+                        if(tPlayer.equalsIgnoreCase("p1")) {
+                            distance = Player.Distance(player1, tX, tY, tZ);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(tPlayer.equalsIgnoreCase("p2")) {
+                            distance = Player.Distance(player2, tX, tY, tZ);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(tPlayer.equalsIgnoreCase("p3")) {
+                            distance = Player.Distance(player3, tX, tY, tZ);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }
+                    }else if(type.equalsIgnoreCase("p")){
+                        String playerA = getNonZeroLengString(scan,"Enter your first player(P1, P2, P3).");
+                        String playerB = getNonZeroLengString(scan, "Enter your second player(P1, P2, P3).");
+                        double distance = 0;
+                        if(playerA.equalsIgnoreCase("p1") && playerB.equalsIgnoreCase("p2")){
+                            distance = Player.PDistance(player1, player2);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(playerA.equalsIgnoreCase("p1") && playerB.equalsIgnoreCase("p3")){
+                            distance = Player.PDistance(player1, player3);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(playerA.equalsIgnoreCase("p2") && playerB.equalsIgnoreCase("p1")){
+                            distance = Player.PDistance(player2, player1);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(playerA.equalsIgnoreCase("p2") && playerB.equalsIgnoreCase("p3")){
+                            distance = Player.PDistance(player2, player3);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(playerA.equalsIgnoreCase("p3") && playerB.equalsIgnoreCase("p1")){
+                            distance = Player.PDistance(player3, player1);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(playerA.equalsIgnoreCase("p3") && playerB.equalsIgnoreCase("p2")){
+                            distance = Player.PDistance(player3, player2);
+                            System.out.printf("%-18s%-3.2f%7s", "The distance is",distance, " units.\n");
+                        }else if(playerA.equalsIgnoreCase(playerB)){
+                            System.out.println("Please choose to different players.");
+                        }
+                    }
                 }
             }else if(yn.equalsIgnoreCase("false")){
                 done = true;
